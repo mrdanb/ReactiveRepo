02c2fe64584e3fd1fcd4641287cfdcd93d073f5b
danbennett <hellodanb@gmail.com>
2020-04-17 11:56:34 +0100

    Fixes issue with missing predicate on FetchPublisher

--- Sources/ReactiveRepo/Publishers/CoreData/NSManagedObjectContext+Fetching.swift
+++ Sources/ReactiveRepo/Publishers/CoreData/NSManagedObjectContext+Fetching.swift
@@ -6,6 +6,7 @@ public extension NSManagedObjectContext {
     func fetchPublisher<T>(of type: T.Type, predicate: NSPredicate?) -> PerformTaskPublisher<[T]> where T: NSManagedObject {
         return PerformTaskPublisher(context: self) { context, finish in
             let request = T.createFetchRequest(resultType: T.self)
+            request.predicate = predicate
             let items = try context.fetch(request)
             finish(items)
         }
